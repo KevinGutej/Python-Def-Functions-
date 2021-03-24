7eec93e7773d981f1e94cfb0a292373d7eea547a
Kevin <kevisrich144@gmail.com>
2021-03-24 16:47:59 +0000

    Define Functions

---
+++ main.py
@@ -0,0 +1,15 @@
+# chelsea=['lampard','Kepa','Rudiger','Alonso','Christ','Jorhino','silva','Kante','Barkley','Abriham','Pulisic','Verner']
+# def chelsea2(team):
+#     for x in team:
+#         print(x)
+# chelsea2(chelsea)
+#
+# def number(number1,number2):
+#     print(number1 * number2)
+#
+# number(2,412321321321)
+#
+# def say(message):
+#     print(message)
+#
+# say("Yo")
